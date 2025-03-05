56425eac380ed3fb1ddcbed0ca2f2e833d45173e
awais-codora <awais.niaz@codora.io>
2025-03-05 11:02:43 +0500

    fix: scapr web data and store in csv file

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/fatih/color"
@@ -18,14 +20,28 @@ func main() {
 
 	defer res.Body.Close()
 
-	doc,err := goquery.NewDocumentFromReader(res.Body)
+	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Fatal("Failed to parse HTML:", err)
 	}
 
-	color.Cyan("Latest Hacker New Articles:");
-	doc.Find("tr").Each(func(i int, s *goquery.Selection) {
-         title:= s.Text()
-		 fmt.Printf("%d. %s\n", i+1, title)
+	file, err := os.Create("hackernews.csv")
+	if err != nil {
+		log.Fatal("Failed to create file:", err)
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{"Title", "URL"})
+	color.Cyan("Latest Hacker New Articles:")
+	doc.Find(".titleline > a").Each(func(i int, s *goquery.Selection) {
+		title := s.Text()
+		link, _ := s.Attr("href")
+		writer.Write([]string{title, link})
+		fmt.Printf("%d. %s\n", i+1, title)
 	})
+	color.Green("✅ Data saved to hackernews.csv")
 }
